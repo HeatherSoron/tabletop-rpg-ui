21176d740f1248997dc9f0b8b6e3e7822cdd9394
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2015-11-11 12:12:37 +1300

    Add weapon slot

--- js/inventory.js
+++ js/inventory.js
@@ -5,8 +5,15 @@ $(function() {
 
 	$('td.inventory').droppable({
 		drop: function(event, ui) {
-			console.log(event, ui);
 			ui.draggable.addClass('in-inventory');
+			ui.draggable.removeClass('in-slot');
+		}
+	});
+
+	$('.slot.weapon').droppable({
+		drop: function(event, ui) {
+			ui.draggable.addClass('in-slot');
+			ui.draggable.removeClass('in-inventory');
 		}
 	});
 });
